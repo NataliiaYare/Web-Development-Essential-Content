4f08a340da341dc6dde532ecfb869644a1bb7eb7
NataliiaYare <natusiazz599@gmail.com>
2024-10-01 15:23:30 +0100

    1.10

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   let mobileClik = document.getElementById("mobile_click");
-//   let navList = document.querySelector(".nav-list");
-
-//   mobileClik.addEventListener("click", function () {
-//     navList.classList.toggle("active");
-//   });
-// });
+// script for mobile menu
 
 const hamMenu = document.querySelector(".ham-menu");
-
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
 hamMenu.addEventListener("click", () => {
